bbad723d9239400b9d4e81815447fa1aee3192b0
Erin Pentecost <erin@pentecost.email>
2018-04-17 18:40:21 -0700

    redis test automation

--- .vscode/runTest.sh
+++ .vscode/runTest.sh
@@ -1,2 +1,17 @@
-cd ./test/Io/Autometa/Lobby/Tests
+ROOTDIR=$(pwd)
+
+cd "${ROOTDIR}"
+cd ./test/Io/Autometa/Redis
+./redis-server &
+sleep 3
+
+cd "${ROOTDIR}"
+cd ./test/Io/Autometa/Lobby/
 dotnet xunit --fx-version 2.0.6
+
+cd "${ROOTDIR}"
+cd ./test/Io/Autometa/Redis
+./redis-cli FLUSHALL
+./redis-cli SHUTDOWN NOSAVE
+
+rm ./dump.rdb
