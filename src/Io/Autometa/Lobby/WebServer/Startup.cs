349e958a1862226ab2e8d916cc3a3d2bfd312522
Erin Pentecost <erin@pentecost.email>
2018-04-27 20:52:30 -0700

    swagger api added

--- src/Io/Autometa/Lobby/WebServer/Startup.cs
+++ src/Io/Autometa/Lobby/WebServer/Startup.cs
@@ -7,11 +7,15 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WebServer
 {
     public class Startup
     {
+        private const string ApiTitle = "Autometa.io Lobby";
+        private const string ApiVersion = "v1";
         public const string AppS3BucketKey = "AppS3Bucket";
 
         public Startup(IConfiguration configuration)
@@ -24,17 +28,30 @@ namespace WebServer
         // This method gets called by the runtime. Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
         {
+            // TODO: Swap with services.AddMvcCore().AddApiExplorer();
             services.AddMvc();
 
             // Add S3 to the ASP.NET Core dependency injection framework.
             services.AddAWSService<Amazon.S3.IAmazonS3>();
+
+
+            // Add Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = ApiTitle, Version = ApiVersion });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            // TODO
             app.UseMvc();
+
+            // Expose swagger middleware for development
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", ApiTitle+" "+ApiVersion);
+            });
         }
     }
 }
