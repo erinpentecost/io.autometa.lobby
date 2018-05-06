cb896f8e0716f1956cce3d14cc7886363fb3798f
Erin Pentecost <erin@pentecost.email>
2018-05-05 20:35:06 -0700

    swagger ui is broke

--- .vscode/publish.sh
+++ .vscode/publish.sh
@@ -3,4 +3,11 @@ ROOTDIR=$(pwd)
 cd "${ROOTDIR}"
 cd ./src/Io/Autometa/Lobby/Server/
 dotnet restore
+dotnet build
+
+cd "${ROOTDIR}"
+cd ./src/Io/Autometa/Lobby/WebServer/
+dotnet restore
+dotnet build
+
 dotnet lambda deploy-function FunctionHandler
--- src/Io/Autometa/Lobby/WebServer/Startup.cs
+++ src/Io/Autometa/Lobby/WebServer/Startup.cs
@@ -11,6 +11,10 @@ using Microsoft.Extensions.Logging;
 using Swashbuckle.AspNetCore;
 using Swashbuckle.AspNetCore.Swagger;
 using Io.Autometa.Lobby.WebServer.Controllers;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.AspNetCore.Http;
+using System.Text;
+using System.IO;
 
 namespace Io.Autometa.Lobby.WebServer
 {
@@ -29,6 +33,8 @@ namespace Io.Autometa.Lobby.WebServer
         // This method gets called by the runtime. Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
         {
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
             services.AddServerTiming();
 
             // TODO: Swap with services.AddMvcCore().AddApiExplorer();
@@ -37,16 +43,33 @@ namespace Io.Autometa.Lobby.WebServer
             // Add S3 to the ASP.NET Core dependency injection framework.
             services.AddAWSService<Amazon.S3.IAmazonS3>();
 
-            // Add Swagger
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("swagger", new Info { Title = ApiTitle, Version = ApiVersion });
+                c.SwaggerDoc(ApiVersion, new Info { Title = ApiTitle, Version = ApiVersion });
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // wrap up lobby exceptions so they are pretty
+            app.Use(async (context, next) =>
+            {
+                try
+                {
+                    await next();
+                }
+                catch (Lobby.Server.LobbyException ex)
+                {
+                    context.Response.StatusCode = ex.HttpCode;
+                    context.Response.Headers["Location"] = "lobby.autometa.io";
+                    context.Response.Body = new MemoryStream();
+                    context.Response.Body.Seek(0, SeekOrigin.Begin);
+                    byte[] data = Encoding.UTF8.GetBytes(ex.Message);
+                    await context.Response.Body.WriteAsync(data, 0, data.Length);
+                }
+            });
+
             // Accept forwarded-IP headers. This allows functionality
             // for users behind NAT, but will also allow for IP spoofing.
             app.UseForwardedHeaders(new ForwardedHeadersOptions
@@ -59,6 +82,12 @@ namespace Io.Autometa.Lobby.WebServer
 
             // Expose swagger middleware for development
             app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                //https://lobby.autometa.io/swagger/v1/swagger.json
+                c.SwaggerEndpoint("/swagger/"+ApiVersion+"/swagger.json", ApiTitle);
+            });
 
             // Should be near the end
             app.UseServerTiming();
